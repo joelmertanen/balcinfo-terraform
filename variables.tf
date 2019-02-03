bf345d0a52990acfa23648350eb8702039eaa7b5
Joel Mertanen <joel@smartly.io>
2019-02-03 18:58:36 +0200

    forgotten variables

---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "region" {}
+variable "external_address" {}
+variable "project_name" {}
+variable "ssh_key_path" {}
+variable "google_cloud_service_account" {}
+variable "service_account_email" {}
